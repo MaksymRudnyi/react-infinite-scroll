06811245e4fd71ec5b04c48f436ef6a2db6de479
Maksym Rudnyi <max.prog.zz@gmail.com>
2023-12-18 20:04:38 +0200

    the same

--- src/useData.js
+++ src/useData.js
@@ -15,7 +15,7 @@ export const useData = () => {
         setIsError(false);
 
         axios
-            .get(`https://openlibrary.org/search.json?q=*&offset=${offset}&limit=${LIMIT}&fields=title,author_name,first_publish_year,cover_i,number_of_pages_median,ratings_average,first_sentence`)
+            .get(`https://openlibrary.org/search.json?q=lord&offset=${offset}&limit=${LIMIT}&fields=title,author_name,first_publish_year,cover_i,number_of_pages_median,ratings_average,first_sentence`)
             .then((res) => {
                 setItems((prevItems) => [...prevItems, ...res.data.docs]);
 
